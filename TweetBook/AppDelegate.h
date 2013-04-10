0960fe92c6e0d1c13d7fc7ceec6cd0487998edfd
Chris O'Neil <cconeil5@gmail.com>
2013-04-10 17:49:49 -0400

    first commit

---
+++ TweetBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TweetBook
+//
+//  Created by Chris O'Neil on 4/10/13.
+//  Copyright (c) 2013 Crash Course. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TweetBook/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  TweetBook
+//
+//  Created by Chris O'Neil on 4/10/13.
+//  Copyright (c) 2013 Crash Course. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
